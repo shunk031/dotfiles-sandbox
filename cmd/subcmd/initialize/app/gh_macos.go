f47706f17f1966fa9a94edc3d303eaacc1c24949
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-08-24 23:38:12 +0900

    add files

--- cmd/subcmd/initialize/app/gh_macos.go
+++ cmd/subcmd/initialize/app/gh_macos.go
@@ -2,10 +2,35 @@
 
 package app
 
-import "github.com/shunk031/dotfiles/cmd/common"
+import (
+	"log"
+	"os/exec"
+
+	"github.com/shunk031/dotfiles/cmd/common"
+)
+
+func installGh() {
+	common.BrewInstall("gh command", "gh", common.BrewOpts{})
+}
+
+func runGhAuthLogin() {
+	cmd := "gh auth login -h github.com -p https"
+
+	c := exec.Command(cmd)
+	err := c.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// err := common.ExecuteCmd(cmd)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+}
 
 func InstallGh() {
 	common.PrintInPurple("\n   Install gh\n")
 
-	common.BrewInstall("gh command", "gh", common.BrewOpts{})
+	installGh()
+	runGhAuthLogin()
 }
---
+++ cmd/subcmd/initialize/app/gh_ubuntu.go
@@ -0,0 +1,7 @@
+//go:build ubuntu
+
+package app
+
+func installGh() {
+
+}
