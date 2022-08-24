abb129648a1d6e597fd4db1657f9451a90f0899c
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-08-25 00:30:32 +0900

    update

--- cmd/subcmd/initialize/common.go
+++ cmd/subcmd/initialize/common.go
@@ -10,6 +10,8 @@ import (
 )
 
 func runSshKeygen() {
+	common.PrintInPurple("\n   Setup for ssh key\n")
+
 	email := "shunsuke.kitada.0831@gmail.com"
 	outputKeyfile := filepath.Join(os.Getenv("HOME"), ".ssh", "id_ed25519")
 	cmd := fmt.Sprintf("ssh-keygen -q -t ed25519 -C \"%s\" -N \"\" -f %s", email, outputKeyfile)
@@ -17,9 +19,24 @@ func runSshKeygen() {
 	common.Execute(msg, cmd)
 }
 
+func installPowerlevel10k() error {
+	dir := filepath.Join(os.Getenv("HOME"), ".zprezto", "modules", "prompt", "external", "powerlevel10k")
+	url := "https://github.com/romkatv/powerlevel10k.git"
+	cmd := fmt.Sprintf("rm -rf %s && git clone --quiet %s %s", dir, url, dir)
+	msg := fmt.Sprintf("Clone to %s", dir)
+	return common.Execute(msg, cmd)
+}
+
+func InstallPowerlevel10k() {
+	common.PrintInPurple("\n   Install powerlevel10k\n")
+	installPowerlevel10k()
+}
+
 func runInitCommon() {
 
 	runSshKeygen()
+
+	InstallPowerlevel10k()
 
 	app.InstallFzf()
 }
