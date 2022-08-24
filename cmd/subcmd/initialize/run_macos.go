344d7ad9d2487afff80492e425d085b42fa6313b
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-08-24 23:38:03 +0900

    update

--- cmd/subcmd/initialize/run_macos.go
+++ cmd/subcmd/initialize/run_macos.go
@@ -3,9 +3,12 @@
 package initialize
 
 func RunInitCmd() error {
+	runInitCommon()
+
 	// err := runInitMacOsCommon()
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
+
 	return runInitMacOsArch()
 }
