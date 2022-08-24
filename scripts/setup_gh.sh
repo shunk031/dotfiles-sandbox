0678c235936228d80b7c3a30ccf13d0e7872d199
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-08-24 23:48:50 +0900

    add script

---
+++ scripts/setup_gh.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function login_to_github() {
+    gh auth login -h github.com -p https
+}
+
+function add_ssh_key_to_github() {
+    gh auth refresh -h github.com -s admin:public_key
+    gh ssh-key add "$HOME"/.ssh/id_ed25519.pub
+}
+
+function main() {
+    login_to_github
+    add_ssh_key_to_github
+}
+
+main
